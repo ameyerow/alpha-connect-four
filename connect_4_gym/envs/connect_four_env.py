a9cf05ddaae4179d941b46ed461b167be2aeac79
MoiisAwesome <45184843+MoiisAwesome@users.noreply.github.com>
2022-04-22 19:38:25 -0400

    added victory checker

--- connect_4_gym/envs/connect_four_env.py
+++ connect_4_gym/envs/connect_four_env.py
@@ -1,12 +1,15 @@
 import gym
 from gym import spaces
 import numpy as np
+from scipy.ndimage import rotate
+
 
 class ConnectFourEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, board_shape=(6, 7), window_width=512, window_height=512):
         super(ConnectFourEnv, self).__init__()
+        self.win_req = 4
         self.board_shape = board_shape
         self.observation_space = spaces.Box(low=-1,
                                             high=1,
@@ -23,3 +26,32 @@ class ConnectFourEnv(gym.Env):
         self.__window_height = window_height
         self.__rendered_board = self._update_board_render()
 
+    # returns winning player where player is as represented in the __board. Player is None if
+    # there is no winner
+    def check_victory(self):
+        for board in [self.__board, np.transpose(self.__board)]:
+
+            # checks rows
+            for i in range(board.shape[0]):
+                result = self._check_row(board[i])
+                if result is not None:
+                    return result
+            # checks diagonal
+            for k in range(-board.shape[0] + 1, board.shape[1]):
+                diagonal = np.diag(board, k=k)
+                if len(diagonal >= self.win_req):
+                    result = self._check_row(diagonal)
+                    if result is not None:
+                        return result
+
+    def _check_row(self, row):
+        player = 0
+        consecutive_count = 0
+        for i in range(len(row)):
+            if row[i] == player:
+                consecutive_count += 1
+            else:
+                player = row[i]
+                consecutive_count = 1
+            if player != 0 and consecutive_count == self.win_req:
+                return player
