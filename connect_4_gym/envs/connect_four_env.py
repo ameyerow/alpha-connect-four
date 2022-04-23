6877b33386aeba7ca634b58c4607b210fe150ed6
Alex Meyerowitz <alexandermeyerowitz@yahoo.com>
2022-04-23 15:45:32 -0400

    Move render into main file, renaming

--- connect_4_gym/envs/connect_four_env.py
+++ connect_4_gym/envs/connect_four_env.py
@@ -1,20 +1,25 @@
 import gym
 from gym import spaces
 import numpy as np
-import pygame
 from scipy.ndimage import rotate
-from render import BoardView
 from time import sleep
+from enum import Enum
 
+class Colors(Enum):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
 
 class ConnectFourEnv(gym.Env):
-    metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human'], 'render_fps':5}
 
     WIN_REWARD = 1
     DRAW_REWARD = 0
     LOSS_REWARD = -1
 
-    def __init__(self, board_shape=(7, 6), window_width=512, window_height=512):
+    def __init__(self, board_shape=(7, 6), screen_size=512):
 
         super(ConnectFourEnv, self).__init__()
         self.win_req = 4
@@ -25,55 +30,55 @@ class ConnectFourEnv(gym.Env):
                                             dtype=int)
         self.action_space = spaces.Discrete(board_shape[1])
 
-        self.__current_player = 1
-        self.__board = np.zeros(self.board_shape, dtype=int)
-        # self.__board = np.random.uniform(-1,1,(7, 6)).round()
+        self.current_player = 1
+        self.board = np.zeros(self.board_shape, dtype=int)
+        # self.board = np.random.uniform(-1,1,(7, 6)).round()
 
-        self.__player_color = 1
-        self.__screen = None
-        self.__window_width = window_width
-        self.__window_height = window_height
+        self.player_color = 1
+        self.screen = None
+        self.clock = None
+        self.screen_size = screen_size
 
     # takes a step with a given action. Returns the new board and the result, which is win, loss, draw, or None if the
     # game has not ended
     def step(self, action):
-        self.insert_into_column(action)
-        winning_player = self.check_victory()
+        self.__insert_into_column(action)
+        winning_player = self.__check_victory()
         if winning_player is not None:
-            if winning_player == self.__current_player:
+            if winning_player == self.current_player:
                 reward = self.WIN_REWARD
             else:
                 reward = self.LOSS_REWARD
         else:
-            if len(self.get_allowed_moves()) == 0:
+            if len(self.__get_allowed_moves()) == 0:
                 reward = self.DRAW_REWARD
             else:
                 reward = None
-        return self.__board.copy(), reward
+        return self.board.copy(), reward
 
 
 
     # returns winning player where player is as represented in the __board. Player is None if
     # there is no winner
-    def check_victory(self):
+    def __check_victory(self):
         # checks rows and columns
-        for board in [self.__board, np.transpose(self.__board)]:
+        for board in [self.board, np.transpose(self.board)]:
             for i in range(board.shape[0]):
-                result = self._check_row(board[i])
+                result = self.__check_row(board[i])
                 if result is not None:
                     return result
 
         # checks diagonals
-        for board in [self.__board, np.rot90(self.__board)]:
+        for board in [self.board, np.rot90(self.board)]:
             for k in range(-board.shape[0] + 1, board.shape[1]):
                 diagonal = np.diag(board, k=k)
                 if len(diagonal >= self.win_req):
-                    result = self._check_row(diagonal)
+                    result = self.__check_row(diagonal)
                     if result is not None:
                         return result
 
     # checks for consecutive pieces from the same player where 0 is empty
-    def _check_row(self, row):
+    def __check_row(self, row):
         player = 0
         consecutive_count = 0
         for i in range(len(row)):
@@ -86,53 +91,89 @@ class ConnectFourEnv(gym.Env):
                 return player
 
     # assumes action is valid; TODO: add move rejection for human players
-    def insert_into_column(self, column):
-        assert(self.__board[0][column] == 0)
+    def __insert_into_column(self, column):
+        assert(self.board[0][column] == 0)
         for i in reversed(range(self.board_shape[0])):
-            if self.__board[i][column] == 0:
-                self.__board[i][column] = self.__current_player
+            if self.board[i][column] == 0:
+                self.board[i][column] = self.current_player
                 return
 
-    def get_allowed_moves(self):
-        return np.nonzero(self.__board[0] == 0)
+    def __get_allowed_moves(self):
+        return np.nonzero(self.board[0] == 0)
 
     def render(self, mode="human", close=False):
         if mode == 'console':
-            replacements = {
-                self.__player_color: 'A',
-                0: ' ',
-                -1 * self.__player_color: 'B'
-            }
-
-            def render_line(line):
-                return "|" + "|".join(
-                    ["{:>2} ".format(replacements[x]) for x in line]) + "|"
-
-            hline = '|---+---+---+---+---+---+---|'
-            print(hline)
-            for line in np.apply_along_axis(render_line,
-                                            axis=1,
-                                            arr=self.__board):
-                print(line)
-            print(hline)
-
+            self.__render_text()
         elif mode == 'human':
-            if self.__screen is None:
-                pygame.init()
-                self.__screen = pygame.display.set_mode(
-                    (round(self.__window_width), round(self.__window_height)))
+            self.__render_gui()
 
-            if close:
-                pygame.quit()
+    def __render_text(self):
+        replacements = {
+            self.player_color: 'A',
+            0: ' ',
+            -1 * self.player_color: 'B'
+        }
 
-            board_view = BoardView(self.__board, self.__window_width)
-            board_view.draw(self.__screen)
-            pygame.display.update()
+        def render_line(line):
+            return "|" + "|".join(
+                ["{:>2} ".format(replacements[x]) for x in line]) + "|"
+
+        hline = '|---+---+---+---+---+---+---|'
+        print(hline)
+        for line in np.apply_along_axis(render_line,
+                                        axis=1,
+                                        arr=self.board):
+            print(line)
+        print(hline)
+    
+    def __render_gui(self):
+        import pygame 
+
+        if self.screen is None:
+            pygame.init()
+            self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        surface = pygame.Surface((self.screen_size, self.screen_size))
+        surface.fill(Colors.WHITE.value)
+
+        longer_board_axis = max(self.board.shape)
+        grid_square_size = self.screen_size // (longer_board_axis)
+
+        if self.board.shape[0] > self.board.shape[1]:
+            x_offset = (self.screen_size - (self.board.shape[1] * grid_square_size))//2
+            y_offset = 0
+        else:
+            y_offset = (self.screen_size - (self.board.shape[0] * grid_square_size))//2
+            x_offset = 0
+
+        for y, row in enumerate(self.board):
+            for x, grid_square in enumerate(row):
+                filled_colors = {
+                    -1: Colors.RED.value,
+                    1 : Colors.BLUE.value,
+                    0 : Colors.WHITE.value  
+                }
+                filled_color = filled_colors[grid_square]
+                frame_color = Colors.YELLOW.value
+
+                pygame.draw.rect(surface, frame_color, 
+                    (grid_square_size*y+y_offset, grid_square_size*x+x_offset, grid_square_size, grid_square_size))
+                
+                grid_square_center = (grid_square_size*(y+.5)+y_offset, grid_square_size*(x+.5)+x_offset)
+                pygame.draw.circle(surface, filled_color, grid_square_center, 0.4*grid_square_size)
+                pygame.draw.circle(surface, Colors.BLACK.value, grid_square_center, 0.4*grid_square_size, width=1)
+
+        pygame.draw.rect(surface, Colors.BLACK.value, 
+            [y_offset, x_offset, self.board.shape[0]*grid_square_size, self.board.shape[1]*grid_square_size], 1)
+    
+        self.screen.blit(surface, (0,0))
+        pygame.display.update()
+        self.clock.tick(self.metadata["render_fps"])
 
 if __name__=="__main__":
     env = ConnectFourEnv()
     env.render()
-    while True:
-        pass
 
 
